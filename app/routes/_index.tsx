5190a5f7f21ba37c6ee1caa98d7ce27639de1230
sebest <sebastianoldani@gmail.com>
2024-01-07 20:13:45 -0300

    first

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,9 +1,36 @@
-import type { MetaFunction } from "@remix-run/node";
+import type { MetaFunction, LinksFunction } from "@remix-run/node";
+import styles from "../styles/index.css"
 
 export const meta: MetaFunction = () => {
   return [
     { title: "New Remix App" },
     { name: "description", content: "Welcome to Remix!" },
+  ];
+};
+
+export const links = () => {
+  return [
+    {
+      rel: "stylesheet",
+      href: "https://necolas.github.io/normalize.css/8.0.1/normalize.css"
+    },
+    {
+      rel: "preconnect",
+      href: "https://fonts.googleapis.com"
+    },
+    {
+      rel: "preconnect",
+      href:"https://fonts.gstatic.com",
+      crossOrigin: "true"
+    },
+    {
+      rel: "stylesheet",
+      href: "https://fonts.googleapis.com/css2?family=Lato:wght@400;700;900&family=Outfit:wght@400;700;900&display=swap"
+    },
+    { 
+      rel: "stylesheet" ,
+      href: styles
+    }
   ];
 };
 
